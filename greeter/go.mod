8b65796268f6a1a65a848e5343f37fcc5762d781
zhouyu <zhouyu@mudu.tv>
2020-06-15 17:51:13 +0800

    'init'

---
+++ greeter/go.mod
@@ -0,0 +1,10 @@
+module github.com/golang-zy/go-demo/go-micro/greeter
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/micro/examples v0.2.0
+	github.com/micro/go-micro/v2 v2.9.0
+	github.com/micro/micro/v2 v2.9.0 // indirect
+)
---
+++ greeter_client/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang-zy/go-demo/go-micro/greeter/proto"
+
+	micro "github.com/micro/go-micro/v2"
+
+)
+
+func main() {
+	// Create a new service
+	service := micro.NewService(micro.Name("greeter.client"))
+	// Initialise the client and parse command line flags
+	service.Init()
+
+	// Create new greeter client
+	greeter := proto.NewGreeterService("greeter", service.Client())
+
+	// Call the greeter
+	rsp, err := greeter.Hello(context.TODO(), &proto.Request{Name: "John"})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Print response
+	fmt.Println(rsp.Greeting)
+}
